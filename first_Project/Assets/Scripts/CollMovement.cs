1be663ba82476a4df0e20187db4e5938ce2411c9
AdharshG <34604785+Adharshg@users.noreply.github.com>
2018-12-19 07:20:16 +0530

    Initial commit

---
+++ first_Project/Assets/Scripts/CollMovement.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+public class CollMovement : MonoBehaviour {
+
+	public Playermovement movement;
+
+	void OnCollisionEnter (Collision collisioninfo){
+		if (collisioninfo.collider.tag=="Obstacle"){
+			movement.enabled=false;
+		}
+	}
+}
---
+++ first_Project/Assets/Scripts/Playermovement.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+public class Playermovement : MonoBehaviour {
+	public Rigidbody rb;
+	public float fforce=2000f;
+	public float sforce=500f;
+	void Start (){
+	}
+	// Update is called once per frame
+	void FixedUpdate () {
+
+			rb.AddForce(0,0,fforce*Time.deltaTime);
+			
+		if(Input.GetKey("d")){
+			rb.AddForce(sforce*Time.deltaTime,0,0,ForceMode.VelocityChange);
+		}
+		
+		if(Input.GetKey("a")){
+			rb.AddForce(-sforce*Time.deltaTime,0,0,ForceMode.VelocityChange);
+		}
+	}
+}
